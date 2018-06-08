2ed7867d22b12e0f337a491b85c6c372cfb397ee
Ravi Bhoraskar <rav@fb.com>
2018-06-07 21:01:23 -0700

    Add a second screen, and simple navigation

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator } from "react-navigation";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+const HomeScreen = props => (
+  <View style={styles.container}>
+    <Text>Welcome to Xeno Canto</Text>
+    <Button
+      title="Search for Bushtit"
+      onPress={() => {
+        props.navigation.navigate("Results", { query: "Bushtit" });
+      }}
+    />
+  </View>
+);
+
+const ResultsScreen = props => (
+  <View style={styles.container}>
+    <Text>{"Searching for " + props.navigation.getParam("query")}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default createStackNavigator(
+  {
+    Home: HomeScreen,
+    Results: ResultsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
